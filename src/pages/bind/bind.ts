c21dfe154eb53837fef9c085eea7a3366df22d63
june0o <cucucy@naver.com>
2018-04-17 21:53:25 +0900

    component, modal page 추가

--- src/pages/bind/bind.ts
+++ src/pages/bind/bind.ts
@@ -7,8 +7,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
-@IonicPage({name:'bindPage'})
+//{name:'bindPage'}
+@IonicPage()
 @Component({
   selector: 'page-bind',
   templateUrl: 'bind.html',
@@ -22,10 +22,18 @@ export class BindPage {
   // life cycle method - 화면이 로드 되고 난 후 호출되는 메서드
   ionViewDidLoad() {
     alert('ionViewDidLoad BindPage' + " " + this.navParams.get("name"));
+    console.log("1. >> Bindpage ionViewDidLoad() 호출")
   }
 
   goBack() {
     this.navCtrl.pop();
   }
 
+  ionViewWillEnter() {
+    console.log("2. >> Bindpage ionViewWillEnter() 호출")
+  }
+
+  ionViewWillUnload() {
+    console.log("3. >> Bindpage ionViewWillUnload() 호출")
+  }
 }
